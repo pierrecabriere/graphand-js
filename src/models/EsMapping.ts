b7608d34182be312ba78471df3531a24f0b1aa61
Pierre Cabrière <hello@pierrecabriere.fr>
2022-02-18 00:15:51 +0100

    add EsMapping defaultQuery

--- src/models/EsMapping.ts
+++ src/models/EsMapping.ts
@@ -1,8 +1,8 @@
+import GraphandFieldBoolean from "../lib/fields/GraphandFieldBoolean";
 import GraphandFieldJSON from "../lib/fields/GraphandFieldJSON";
 import GraphandFieldScope from "../lib/fields/GraphandFieldScope";
 import GraphandFieldText from "../lib/fields/GraphandFieldText";
 import GraphandModel from "../lib/GraphandModel";
-import GraphandFieldBoolean from "../lib/fields/GraphandFieldBoolean";
 
 class EsMapping extends GraphandModel {
   static apiIdentifier = "elasticsearch";
@@ -19,6 +19,14 @@ class EsMapping extends GraphandModel {
       externalHost: new GraphandFieldBoolean({ name: "Utiliser un hôte externe" }),
       host: new GraphandFieldText({ name: "Hôte" }),
       conditions: new GraphandFieldJSON({ name: "Conditions" }),
+      defaultQuery: new GraphandFieldJSON({
+        name: "Requête par défaut",
+        defaultValue: {
+          query_string: {
+            query: "*{q}*",
+          },
+        },
+      }),
     };
   }
 
@@ -39,9 +47,20 @@ class EsMapping extends GraphandModel {
 
   async search(query) {
     const { constructor } = Object.getPrototypeOf(this);
+
+    let req;
+    if (typeof query === "string") {
+      req = constructor._client._axios.get(`/elasticsearch/${this._id}/search`, {
+        params: { q: query },
+      });
+    } else {
+      req = constructor._client._axios.post(`/elasticsearch/${this._id}/search`, query);
+    }
+
     const {
       data: { data },
-    } = await constructor._client._axios.post(`/elasticsearch/${this._id}/search`, query);
+    } = await req;
+
     return data;
   }
 
